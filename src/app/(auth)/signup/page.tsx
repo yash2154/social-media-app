135f4e6adb78cf1013ea95a39df8d293bc61c895
yash rai <yash2154rai@gmail.com>
2024-07-22 18:08:10 +0530

    add login and signup actions

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,40 @@
+import { Metadata } from "next";
+import signupImage from "@/assets/signup-image.jpg";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Sign up",
+};
+
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center p-5 ">
+      <div className="  flex h-full max-h-[40rem] w-full max-w-[64rem] rounded-2xl overflow-hidden bg-card shadow-2xl">
+        <div className=" md:w-1/2  w-full space-y-10 overflow-y-auto p-10 ">
+          <div className=" space-y-1 text-center">
+            <h1 className="text-3xl font-bold">Sign Up to bugbook</h1>
+            <p className=" text-muted-foreground">
+              A place where even <span className="italic">you</span> can find a
+              friend.
+            </p>
+          </div>
+          <div className=" space-y-5">
+            sign up form
+            <Link
+              href={"/login"}
+              className=" block text-center hover:underline"
+            >
+              Already have an account?
+            </Link>
+          </div>
+        </div>
+        <Image
+          src={signupImage}
+          alt=""
+          className="w-1/2 hidden md:block object-cover"
+        ></Image>
+      </div>
+    </main>
+  );
+}
