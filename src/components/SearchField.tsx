cd1c7f689e0de0df12733ee49616b35c02558644
yash rai <yash2154rai@gmail.com>
2024-07-25 17:32:37 +0530

    add functinality to search field

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -6,9 +6,15 @@ import { SearchIcon } from "lucide-react";
 
 export default function SearchField() {
   const router = useRouter();
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {}
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const q = (form.q as HTMLInputElement).value.trim();
+    if (!q) return;
+    router.push(`/search?q=${encodeURIComponent(q)}`);
+  }
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} method="GET" action={"/search"}>
       <div className="relative">
         <Input name="q" placeholder="Search" className="pe-10" />
         <SearchIcon className="absolute right-3 top-1/2 -translate-y-1/2 transform text-muted-foreground"></SearchIcon>
